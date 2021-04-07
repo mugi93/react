63ec7158259b9171d82c70833b27b57d51bed78a
Mugi <g.mugi93140@gmail.com>
2021-04-07 17:30:48 +0200

    class

--- chapitre-01/elsy/src/App.js
+++ chapitre-01/elsy/src/App.js
@@ -1,29 +1,40 @@
 import React from "react";
 import './App.css';
-import'./components/Steps';
-import Home from "./components/Steps";
+
+import Home from "./components/box";
 import './styles/global.css'
-const tempMin=-20
-const tempMax =40 
-const heartMin =80
-const heartMax =180
-const stepsMin =0
-const stepsMAx =5000
+
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMAx = 5000
 
 class App extends React.Component {
   render() {
     return (
       <div class="container-fluid">
-      <div class="row">
-        <Home/>
-        <p>Heart : {heartMin}</p>
-        <p>Temperature :{tempMin}</p>
-        <p>Steps :{stepsMin}</p>
-        
+        <div class="row">
+          <Home>
+            {this.props.icon = "local_drinks"}
+            {this.props.color = "#3A85FF"}
+            {this.props.value = 1.5}
+            {this.props.unit = "L"}
+            </Home>       {/* water */}
+            
+            
+            <Home></Home>        {/*Steps  */}
+            <Home></Home>        {/*heart  */}
+            <Home></Home>          {/* temperature */}
+            <p>Heart : {heartMin}</p>
+            <p>Temperature :{tempMin}</p>
+            <p>Steps :{stepsMin}</p>
+        </div>
 
 
-      </div>
-        
+      
+
       </div>
     );
   }
--- chapitre-01/elsy/src/components/box.jsx
+++ chapitre-01/elsy/src/components/box.jsx
@@ -6,7 +6,9 @@ class Home extends React.Component {
   render() {
     return (
 			<div class="box col-sm-3 col-6" >
-				<span class="material-icons" style={{fontSize:100,color:"black"}}>directions_walk</span>
+				<span class="material-icons" style={{fontSize:100,color:this.props.color}}>{this.props.icon}</span>
+				<p>{this.props.value}</p>
+				<p>{this.props.unit}</p>
 			</div>
 		)
   };
