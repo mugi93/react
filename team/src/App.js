31fedfd8db6018b40a1b07d8acfff83a85984c63
Mugi <g.mugi93140@gmail.com>
2021-04-08 18:05:32 +0200

    point

--- team/src/App.js
+++ team/src/App.js
@@ -5,21 +5,33 @@ import City from"./city/city.js";
 
 const listTeam=[ 
 
-  {name:"Arsenal",colors:"red"},
-  {name:"Manchester United",colors:"red"},
-  {name:"Manchester City",colors:"purple"},
-  {name:"Tottenham",colors:"white"},
+  {name:"Arsenal",colors:"red",number:4,point:5},
+  {name:"Manchester United",colors:"red",number:4,point:15},
+  {name:"Manchester City",colors:"purple",number:4,point:20},
+  {name:"Tottenham",colors:"white",number:4,point:10},
 ];
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      amount:listTeam.point
+    }
+  }
+
+
+
+
   render(){
     return (
       <div>
         <h1 style={{color:"red"}}>Liste d'equipe:</h1>
         {listTeam.map((elem)=>{console.log(elem)
-          return (<City color={elem.colors} nameTeam={elem.name}/>)
+          return (<City color={elem.colors} nameTeam={elem.name} match={elem.number} amount={elem.point}/>)
         })}
-       
+       <button onClick={}>
+
+       </button>
       </div>
 
     );
--- team/src/city/city.js
+++ team/src/city/city.js
@@ -6,6 +6,8 @@ class City extends React.Component{
             <div>
                 <h2 style={{color:this.props.color,backgroundColor:"black"}}>Team is: {this.props.nameTeam} and it is colors: {this.props.color}</h2>
                 
+                <h4>The Amount of point is:{this.props.amount}</h4>
+                <h4>Number of Matches:{this.props.match}</h4>
                 
             
 
